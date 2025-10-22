789e2ba262c6d02a39b37255a900d9729b5639d5
livwilliamson4 <ow2104@gmail.com>
2025-10-22 13:22:50 +1100

    Update detect_beacon.py

--- detect_beacon.py
+++ detect_beacon.py
@@ -225,13 +225,13 @@ class DetectBeacon(Node):
         else:
             self.stop_cmd = False
 
-        # Normalising stripe width (should be [90,190]) to [0,1]. 
+        # Normalising stripe width (should be [140,190]) to [0,1]. 
         if self.stripe_width > 190 or self.stripe_width == 0: # If wider than 190 pixels (closer than min following distance) or can't see, set point to zero
             self.norm_stripe = 0
         #elif self.stripe_width < 90:
             #self.norm_stripe = 1
         else:
-            self.norm_stripe = (self.stripe_width - 190)/100
+            self.norm_stripe = (self.stripe_width - 190)/50
 
         # Normalising centre of user (should be [0,640]) to [-1,1]. camera width = 640, therefore centre = 640/2 = 320
         self.norm_user = (self.centre_of_user - 320)/320
@@ -247,7 +247,7 @@ class DetectBeacon(Node):
         # PID loops, more aggressive PID loop for when trailer needs to stop
         pid_stop = PID(1, 0.1, 0.05, setpoint=0)
         pid_stripe = PID(0.5, 0.1, 0.05, setpoint=0) # 0 is vest at 190 pixels wide which is ideal following distance
-        pid_steering = PID(0.7, 0.1, 0.05, setpoint=0) # 0 is user at centre which is ideal
+        pid_steering = PID(0.9, 0.1, 0.05, setpoint=0) # 0 is user at centre which is ideal
 
         if self.stop_cmd == True:
             self.speed = round(pid_stop(self.norm_stripe), 3)
@@ -258,7 +258,7 @@ class DetectBeacon(Node):
         # Keeping speed signal within [0,1] range
         if self.speed > 1:
             self.speed = 1
-        elif self.speed < 0:
+        elif self.speed < 0.2:
             self.speed = 0
 
         #self.get_logger().info(f'self.speed = {self.speed}')
